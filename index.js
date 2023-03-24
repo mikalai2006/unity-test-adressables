6efd5668ee933c131ef0d15e67a1f51f5cb1475f
Mikalai Parakhnevich <composer.opencart@gmail.com>
2023-03-24 16:00:47 +0300

    feat: add index.html as entrypoint app

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const PORT = process.env.PORT || 5001;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
+  .get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
