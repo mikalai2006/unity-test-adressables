a78425e9da71ecfaba62fe488e6405eb1b175923
Mikalai Parakhnevich <composer.opencart@gmail.com>
2023-04-03 12:15:27 +0300

    feat: change port

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /usr/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 5001
+EXPOSE 5555
 # RUN npm run ts-build
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5555;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
